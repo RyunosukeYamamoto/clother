4391b2665d075dbc5934d2c6d072bf3ede3a3a05
Ubuntu <ubuntu@ip-172-31-63-18.ec2.internal>
2021-02-18 11:09:41 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :users, only: [:create, :show, :edit, :destroy]
 end
